899635ed739c8c8988a2ba6543ed9af15637713b
Khensane Paulo <90350372+khensanepaulo@users.noreply.github.com>
2023-05-04 21:07:40 -0300

    Initial commit

---
+++ Greenployee/Model/OrdemServico.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Greenployee.Model
+{
+    public class OrdemServico:BaseClass
+    {
+        [StringLength(15)]
+        public string nrOrdem { get; set; } = string.Empty;
+        [StringLength(60)]
+        public string nmCliente { get; set; } = string.Empty;
+        [StringLength(20)]
+        public string nrTelefone { get; set;} = string.Empty;
+        [StringLength(15)]
+        public string flSituacao { get; set; } = string.Empty;
+        [StringLength(15)]
+        public string dsFormaPagamento { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string dsEndereco { get; set; } = string.Empty;
+        public bool flEntrega { get; set; }
+        public DateTime dtOrdem { get; set; }
+        public int idFuncionario { get; set; }
+        public Pessoa? Funcionario { get; set; }
+    }
+}
---
+++ GreenployeeClient/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
