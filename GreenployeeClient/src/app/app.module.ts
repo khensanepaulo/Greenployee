591a8cf9faa8225fdda0e862f725ac57d79b8847
Gustavo <gustavo.koslovski2001@gmail.com>
2023-05-29 19:30:02 -0300

    tela inicio

--- GreenployeeClient/src/app/app.module.ts
+++ GreenployeeClient/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { InicioComponent } from './views/inicio/inicio.component';
+import { RelatorioComponent } from './views/relatorio/relatorio.component';
+import { ModalComponentComponent } from './views/modal-component/modal-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InicioComponent
+    InicioComponent,
+    RelatorioComponent,
+    ModalComponentComponent,
   ],
   imports: [
     BrowserModule,
--- GreenployeeClient/src/app/views/inicio/inicio.component.ts
+++ GreenployeeClient/src/app/views/inicio/inicio.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class InicioComponent {
 
+  listaFake: any[] = [
+    { id: 1, nome: 'Item 1', valor: 100 },
+    { id: 2, nome: 'Item 2', valor: 200 },
+    { id: 3, nome: 'Item 3', valor: 300 },
+    { id: 4, nome: 'Item 3', valor: 300 },
+    { id: 5, nome: 'Item 3', valor: 300 },
+    { id: 6, nome: 'Item 3', valor: 300 },
+    // ...
+  ];
+ 
 }
+
+
+
+
+
